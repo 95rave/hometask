7a383c2825e59935a01933b6d73bbdbd7cd807a9
95rave <ravanaap@code.edu.az>
2022-04-13 16:05:58 +0400

    first task

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pieces = 0;
+            pieces = int.Parse(Console.ReadLine());
+            if (pieces % 2 == 0)
+                Console.WriteLine(pieces+ "is a double");
+            else
+                Console.WriteLine(pieces+ "is a sinle");
+             
+           
+          
+        }
+    }
+}
